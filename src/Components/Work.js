06d668c4af48222830900a655a6e1574e7aaa602
Franchesca <ihuoma.franchesca@gmail.com>
2022-12-07 19:28:30 +0100

    added some more projects

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -4,6 +4,8 @@ import Health from "../Assets/health.png";
 import Food from "../Assets/food.png";
 import Todo from "../Assets/todo.png";
 import Gym from "../Assets/gym.png";
+import Maldives from "../Assets/maldives.jpg";
+import Fashion from "../Assets/fashion.webp";
 import Project from "../Assets/project.png";
 
 const Work = () => {
@@ -152,6 +154,55 @@ const Work = () => {
               </div>
             </div>
           </div>
+
+          <div
+            style={{ backgroundImage: `url(${Maldives})` }}
+            className="shadow-lg shadow-[#040c16] group round-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://travel-website-by-ify.netlify.app/">
+                  <button className="rounded-lg px-2 py-2 m-2 bg-[#0a192f] text-yellow-600 font-bold text-lg">
+                    View Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Franchesca02/Travel-Website">
+                  <button className="rounded-lg px-2 py-2 m-2 bg-[#0a192f] text-yellow-600 font-bold text-lg">
+                    View Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Fashion})` }}
+            className="shadow-lg shadow-[#040c16] group round-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://consuming-api-with-js.netlify.app/">
+                  <button className="rounded-lg px-2 py-2 m-2 bg-[#0a192f] text-yellow-600 font-bold text-lg">
+                    View Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Franchesca02/JavaScript-API-Consumption">
+                  <button className="rounded-lg px-2 py-2 m-2 bg-[#0a192f] text-yellow-600 font-bold text-lg">
+                    View Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
